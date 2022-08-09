dc14c847e12d83271ec762823180e112d62e6676
Vinicius <viniciusnemtrindade@gmail.com>
2022-08-09 06:32:31 -0300

    Criação do Esqueleto da Aplicação - segunda parte: iniciando frontend Angular, adicionando requisições Http

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,6 +27,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("Default Connection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 

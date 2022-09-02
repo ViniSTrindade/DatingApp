72ed46e80df3e4b220454095b6040d6bce64da09
Vinicius <viniciusnemtrindade@gmail.com>
2022-09-02 03:49:08 -0300

    Criando ITokenService Interface, para facilitar testes e manter boas práticas.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,7 @@
 
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -20,9 +22,10 @@ namespace API
      
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services){
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options => {
                 options.UseSqlite(_config.GetConnectionString("Default Connection"));
             });

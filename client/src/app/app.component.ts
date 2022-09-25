2dfdd855e735612990296fbaf5e405971a5c8dcf
Vinicius <viniciusnemtrindade@gmail.com>
2022-09-25 06:26:51 -0300

    Usando comunicação de pai para filho entre componentes Home e Register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -12,22 +11,14 @@ export class AppComponent implements OnInit{
   users: any;
   title = 'Dating App';
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7263/api/users').subscribe({
-      next: (response) => this.users = response,
-      error: (e) => console.error(e)
-    })
   }
 }

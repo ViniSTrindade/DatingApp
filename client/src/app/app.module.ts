2558b3051db76ba795287acbdc98319d8d2f89a5
Vinicius <viniciusnemtrindade@gmail.com>
2022-09-28 07:26:10 -0300

    Adicionando serviço de Toast e Proteção de Rotas Restritas

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,39 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
 import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NavComponent } from './nav/nav.component';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HomeComponent,
-    RegisterComponent,
     ListsComponent,
-    MessagesComponent,
-    MemberListComponent,
     MemberDetailComponent,
+    MemberListComponent,
+    MessagesComponent,
+    NavComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    BsDropdownModule.forRoot(),
     FormsModule,
-    BsDropdownModule.forRoot()
-
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
